f9f93b9c36433df07ca60234417abf98c6cf0ac4
sandeepspd123 <sandeepsky708@gmail.com>
2025-03-26 01:10:21 +0530

    create navbar and frontend changes

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,16 +1,22 @@
 //= link_tree ../images
 //= link_tree ../builds
 
-//= link style.css
-//= link frontend.css
+//= link frontend-first.css
+//= link frontend-second.css
 //= link frontend-icon.css
+//= link frontend-third.css
+//= link style.css
 //= link frontend-essential.css
 //= link gutenverse-content-29-963903.css
 //= link all.min.css
- //= link gtnicon.css
+//= link gtnicon.css
 
+
+
+//= link corefrontend.js
+//= link frontend-first.js
+//= link frontend-second.js
+//= link profrontend.js
+//= link frontend-third.js
 //= link email-decode.min.js
 //= link application.js
-//= link frontend.js
-//= link corefrontend.js
-//= link profrontend.js
