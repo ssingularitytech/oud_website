199ee4e042b52ba68afa44f4fc61ff47281eb500
sandeepspd123 <sandeepsky708@gmail.com>
2025-03-28 08:41:39 +0530

    create sscratch landing page and navbar

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,35 +2,4 @@ class LandingController < ApplicationController
   def index
   end
 
-  def about
-  end
-
-  def team
-  end  
-
-  def projects
-  end 
-  
-  def music
-  end 
-
-  def ticket
-  end 
-
-  def faq
-  end 
-
-  def news
-  end 
-
-  def singlenews
-  end
-
-  def contact
-  end
-
-  def calendar
-  end
-
-
 end
