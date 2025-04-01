83178dd9412dbd9e3b58034fe16bdb239cc9f740
sandeepspd123 <sandeepsky708@gmail.com>
2025-04-02 00:46:58 +0530

    install cancancan and create role admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def role
+    email == "admin@example.com" ? "admin" : "user"
+  end
+
+  def admin?
+    role == "admin"
+  end
 end
