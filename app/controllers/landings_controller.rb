69d682d2169cc767a386fc115f274a876424ab34
sandeepspd123 <sandeepsky708@gmail.com>
2025-05-29 14:35:06 +0530

    add video card in home page

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,6 +1,7 @@
 class LandingsController < ApplicationController
   def index
    @upcoming_events = UpcomingEvent.order(date: :asc).limit(5)
+   @videos = Video.order(created_at: :desc).limit(1)
   end
 
   def about
