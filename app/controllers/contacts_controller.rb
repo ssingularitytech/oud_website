f35f06225623bff2a468cf1479256b6277b3b1c2
sandeepspd123 <sandeepsky708@gmail.com>
2025-06-06 14:28:12 +0530

    add notification in send contact message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,10 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      flash[:notice] = 'Your message has been sent successfully!'
-      redirect_to contacts_path
+      redirect_to contacts_path, notice: 'Your message has been sent successfully!'
     else
-      flash[:alert] = 'There was an error sending your message. Please try again.'
-      render :new
+      flash.now[:alert] = 'There was an error sending your message. Please try again.'
+      render :new, status: :unprocessable_entity
     end
   end
   private
