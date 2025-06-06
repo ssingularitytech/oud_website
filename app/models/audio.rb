423b9d483e5a44deb66ef67598b1b99e9cfb465a
sandeepspd123 <sandeepsky708@gmail.com>
2025-06-06 15:30:12 +0530

    changes in new release page

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,4 +1,5 @@
 class Audio < ApplicationRecord
   belongs_to :music
   has_one_attached :audio
+  default_scope { order(id: :asc) }
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ApplicationRecord
   has_one_attached :photo
+  default_scope { order(id: :asc) }
 end
