b07ed571772915fd6c0327fcbface0bfcb87bf5d
sandeepspd123 <sandeepsky708@gmail.com>
2025-06-25 17:23:57 +0530

    chnages in event page

--- app/controllers/upcoming_events_controller.rb
+++ app/controllers/upcoming_events_controller.rb
@@ -1,19 +1,10 @@
 class UpcomingEventsController < ApplicationController
 
   def index
-    if params[:upcoming_ensemble].present?
-      # Search in upcoming events
-      @upcoming_events = UpcomingEvent.where("date >= ? AND LOWER(ensemble) LIKE ?", Date.today, "%#{params[:upcoming_ensemble].downcase}%")
-    else
-      @upcoming_events = UpcomingEvent.where("date >= ?", Date.today)
-    end
-
-    if params[:past_ensemble].present?
-      # Search in past events
-      @past_events = UpcomingEvent.where("date < ? AND LOWER(ensemble) LIKE ?", Date.today, "%#{params[:past_ensemble].downcase}%")
-    else
-      @past_events = UpcomingEvent.where("date < ?", Date.today)
-    end
+    # Add debugging to verify data
+    @upcoming_events = UpcomingEvent.upcoming
+    @past_events = UpcomingEvent.past
+    
     Rails.logger.debug "Upcoming Events: #{@upcoming_events.map(&:date)}"
     Rails.logger.debug "Past Events: #{@past_events.map(&:date)}"
   end
