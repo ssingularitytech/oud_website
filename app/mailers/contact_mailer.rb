2d77574d8f01d54663ea8f0b03aa1f500f15fb22
sandeepspd123 <sandeepsky708@gmail.com>
2025-06-25 13:12:09 +0530

    fix the email contact submission

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,7 @@ class ContactMailer < ApplicationMailer
   def send_contact_mail(contact)
     @contact = contact
     mail(
-      to: 'info@nizarrohana.com',
+      to: 'sandeepsky708@gmail.com',
       subject: 'New Contact Us Submission',
       reply_to: @contact.email  # Add this to allow replies to the contact
     )
