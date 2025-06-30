c4185d9ef7dff87edd8cd570140b24252ddd8169
sandeepspd123 <sandeepsky708@gmail.com>
2025-06-30 12:56:42 +0530

    changes in event page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
+  def add_target_blank_to_links(html)
+    # parse the HTML
+    fragment = Loofah.fragment(html)
+
+    fragment.css('a').each do |link|
+      link['target'] = '_blank'
+      link['rel'] = 'noopener'
+    end
+
+    fragment.to_s.html_safe
+  end
 end
