1db3fff5804dcb8dfd22d3006a517ee075dae002
Deepak <deepakpanigrahi7873@gmail.com>
2025-06-30 13:15:05 +0530

    fix the mails to save in the subscribe model

--- app/controllers/subscribes_controller.rb
+++ app/controllers/subscribes_controller.rb
@@ -1,25 +1,20 @@
 class SubscribesController < ApplicationController
-
   def new
     @subscribe = Subscribe.new
   end
-  
+
   def create
     @subscribe = Subscribe.new(subscribe_params)
     if @subscribe.save
-      redirect_to subscribes_path, notice: 'Your message has been sent successfully!'
-      flash[:notice] = "Subscribed successfully!"
+      redirect_to contacts_path, notice: "Subscribed successfully!"
     else
-      flash[:alert] = "Subscription failed."
+      redirect_back fallback_location: root_path, alert: "Subscription failed."
     end
-    redirect_back fallback_location: root_path
   end
-
 
   private
 
   def subscribe_params
     params.require(:subscribe).permit(:email)
-
   end
 end
