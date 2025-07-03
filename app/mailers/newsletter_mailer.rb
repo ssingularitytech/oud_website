6c03bb9ebea722d90ef76c8fe6ea5951bfd6b4d5
Deepak <deepakpanigrahi7873@gmail.com>
2025-07-03 14:58:19 +0530

    add newslatter and send a mail to Subscribe mails

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -3,10 +3,28 @@ class NewsletterMailer < ApplicationMailer
 
   def send_newsletter(newsletter, subscriber_email)
     @newsletter = newsletter
+    @subscriber_email = subscriber_email
+
+    # Attach and embed file if present
+    if @newsletter.file.attached? && @newsletter.file.blob&.present?
+    attachment = @newsletter.file
+
+    if attachment.image?
+        attachments.inline[attachment.filename.to_s] = {
+        mime_type: attachment.content_type,
+        content: attachment.download
+        }
+    else
+        attachments[attachment.filename.to_s] = {
+        mime_type: attachment.content_type,
+        content: attachment.download
+        }
+    end
+    end
+
     mail(
       to: subscriber_email,
-      subject: "New Newsletter" # fallback subject
+      subject: "New Newsletter"
     )
   end
-
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -571,33 +571,50 @@ end
     end
 
 
-    config.model 'Newsletter' do
-      edit do
-        field :file, :active_storage
-      end
+config.model 'Newsletter' do
+  edit do
+    field :file, :active_storage
+  end
 
-      list do
-        field :id
-        field :file do
-          pretty_value do
-            if bindings[:object].file.attached?
-              bindings[:view].link_to(bindings[:object].file.filename.to_s, Rails.application.routes.url_helpers.rails_blob_path(bindings[:object].file, only_path: true))
-            end
-          end
-        end
-        field :created_at
-      end
-
-      show do
-        field :file do
-          pretty_value do
-            if bindings[:object].file.attached?
-              bindings[:view].link_to(bindings[:object].file.filename.to_s, Rails.application.routes.url_helpers.rails_blob_path(bindings[:object].file, only_path: true))
-            end
-          end
+  list do
+    field :id
+    field :file do
+      pretty_value do
+        if bindings[:object].file.attached? && bindings[:object].file.image?
+          bindings[:view].tag(:img,
+            src: Rails.application.routes.url_helpers.rails_blob_path(bindings[:object].file, only_path: true),
+            style: 'max-width: 100px; max-height: 100px;'
+          )
+        elsif bindings[:object].file.attached?
+          bindings[:view].link_to(
+            bindings[:object].file.filename.to_s,
+            Rails.application.routes.url_helpers.rails_blob_path(bindings[:object].file, only_path: true)
+          )
         end
       end
     end
+    field :created_at
+  end
+
+  show do
+    field :file do
+      pretty_value do
+        if bindings[:object].file.attached? && bindings[:object].file.image?
+          bindings[:view].tag(:img,
+            src: Rails.application.routes.url_helpers.rails_blob_path(bindings[:object].file, only_path: true),
+            style: 'max-width: 300px; max-height: 300px;'
+          )
+        elsif bindings[:object].file.attached?
+          bindings[:view].link_to(
+            bindings[:object].file.filename.to_s,
+            Rails.application.routes.url_helpers.rails_blob_path(bindings[:object].file, only_path: true)
+          )
+        end
+      end
+    end
+  end
+end
+
 
   end
 end
